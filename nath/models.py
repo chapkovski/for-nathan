4fd27ea93f000943eeeac17fa4b742285c3d5d00
chapkovski <chapkovski@gmail.com>
2017-08-15 12:16:41 +0200

    first commit

---
+++ nath/models.py
@@ -0,0 +1,63 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+import random
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'risk_aversion'
+    players_per_group = None
+    num_rounds = 1
+    num_gambles = 10
+    risk_takers_payoff_1 = c(10)
+    risk_takers_payoff_2 = c(0.25)
+    risk_averse_payoff_1 = c(5)
+    risk_averse_payoff_2 = c(4)
+    lb = 1
+    ub = 10
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+
+class Player(BasePlayer):
+
+    gamble_num = models.IntegerField()
+    num_drawn = models.IntegerField()
+    def get_lot_choices(self):
+        choice = getattr(self,
+                         "gamble_{}".format(self.gamble_num))
+        if not choice:
+            lottery_choices = [Constants.risk_takers_payoff_1,
+                               Constants.risk_takers_payoff_2]
+
+        else:
+            lottery_choices = [Constants.risk_averse_payoff_1,
+                               Constants.risk_averse_payoff_2]
+        return lottery_choices
+
+    def set_payoffs(self):
+        self.gamble_num = random.choice(range(1, Constants.num_gambles + 1))
+        self.num_drawn = random.choice(range(Constants.lb, Constants.ub + 1))
+        if self.num_drawn < self.gamble_num:
+            self.payoff = self.get_lot_choices()[0]
+        else:
+            self.payoff = self.get_lot_choices()[1]
+
+
+for i in range(1, Constants.num_gambles+1):
+    Player.add_to_class("gamble_{}".format(i),
+                        models.BooleanField())
---
+++ nath/views.py
@@ -0,0 +1,76 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from django.forms import Field
+from django.utils.translation import ugettext_lazy
+from .models import Constants
+from django import forms
+from django.forms import Field
+from django.utils.translation import ugettext_lazy
+
+
+class Introduction(Page):
+    def is_displayed(self):
+        return self.subsession.round_number == 1
+
+class Instruction(Page):
+    pass
+
+class Instruction2(Page):
+    pass
+
+class Decisions(Page):
+    form_model = models.Player
+    form_fields = ['gamble_{}'.format(i)
+                   for i in range(1, Constants.num_gambles + 1)]
+
+    def vars_for_template(self):
+        smth = self.get_form()
+        fields_to_show = self.form_fields
+        leftcol = []
+        rightcol = []
+        maxlen = len(fields_to_show) + 1
+        for i, f in enumerate(fields_to_show):
+            str1 = ', '.join(str(e) for e in [_ for _ in range(1, i+2)])
+            str2 = ', '.join(str(e) for e in [_ for _ in range(i+2, maxlen )])
+            leftcol.append(str1)
+            rightcol.append(str2)
+
+        return {'abclist': zip(smth, leftcol, rightcol), }
+
+    def before_next_page(self):
+        self.player.set_payoffs()
+
+
+class ResultsWaitPage(WaitPage):
+
+    def after_all_players_arrive(self):
+        pass
+
+
+class Results2(Page):
+    def vars_for_template(self):
+        choice = getattr(self.player,
+                         "gamble_{}".format(self.player.gamble_num))
+        maxlen = Constants.num_gambles + 1
+        str1 = ', '.join(str(e) for e in [_ for _ in range(1, self.player.gamble_num+1)])
+        str2 = ', '.join(str(e) for e in [_ for _ in range(self.player.gamble_num+1, maxlen)])
+        return {'chosen_gamble_choice': choice,
+                'str1': str1,
+                'str2': str2,
+                }
+        pass
+
+
+page_sequence = [
+    # Introduction,
+    # Instruction,
+    # Instruction2,
+    Decisions,
+    Results2
+]
